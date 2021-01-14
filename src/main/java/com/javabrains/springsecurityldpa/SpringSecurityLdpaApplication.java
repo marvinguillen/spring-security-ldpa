9e0db51d73120f82cf6c11c7a82fd9206369ab1b
marvin.a.guillen@gmail.com <marvin.a.guillen@gmail.com>
2021-01-13 20:12:46 -0600

    First commit to ldap security

---
+++ src/main/java/com/javabrains/springsecurityldpa/SpringSecurityLdpaApplication.java
@@ -0,0 +1,13 @@
+package com.javabrains.springsecurityldpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityLdpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityLdpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javabrains/springsecurityldpa/SpringSecurityLdpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javabrains.springsecurityldpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLdpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
